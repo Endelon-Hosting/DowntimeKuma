5c4f9b395c4dd879c5b37613f33884ba5eaef1bb
Marcel Baumgartner <marcel.kbkm@gmail.com>
2022-04-18 00:21:30 +0200

    Added ping module

---
+++ DowntimeKuma/Core/DowntimeKuma/MonitorModules/PingModule.cs
@@ -0,0 +1,50 @@
+﻿using Logging.Net;
+
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace DowntimeKuma.Core.DowntimeKuma.MonitorModules
+{
+    public class PingModule : AbstractMonitorModule
+    {
+        public override string Id { get { return "ping"; } }
+
+        public override MonitorData Monitor(Monitor m)
+        {
+            try
+            {
+                var ping = new Ping();
+                var reply = ping.Send(m.Target);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    return new MonitorData()
+                    {
+                        Success = true,
+                        Error = $"Ping: {reply.RoundtripTime}ms"
+                    };
+                }
+                else
+                    return new MonitorData()
+                    {
+                        Error = $"Ping failed: {reply.Status}",
+                        Success = false
+                    };
+            }
+            catch (Exception e)
+            {
+                return new MonitorData()
+                {
+                    Error = e.Message,
+                    Success = false
+                };
+            }
+        }
+
+        public override void Prepare()
+        {
+            Logger.Info($"Loaded '{Id}' module");
+        }
+    }
+}
